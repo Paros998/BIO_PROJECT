cd7a8d90113011ad817e0b21843a599693ed4004
komob <b.komorniczak@gmail.com>
2024-11-11 17:02:47 +0100

    add reservation service

--- psk.bio.car.rental.application/src/main/java/psk/bio/car/rental/application/reservation/ReservationRepository.java
+++ psk.bio.car.rental.application/src/main/java/psk/bio/car/rental/application/reservation/ReservationRepository.java
@@ -1,6 +1,9 @@
 package psk.bio.car.rental.application.reservation;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import psk.bio.car.rental.application.user.UserProjection;
 
 public interface ReservationRepository {
@@ -8,4 +11,10 @@ public interface ReservationRepository {
   UserProjection findEmployee();
   LocalDate findDateOdRent();
   LocalDate findDateOfReturn();
+  Optional<ReservationProjection> findReservationById(UUID id);
+  List<ReservationProjection> findAllReservations();
+  ReservationProjection save(ReservationProjection reservationProjection);
+  ReservationProjection update(ReservationProjection reservationProjection);
+  void delete(ReservationProjection reservationProjection);
+
 }
---
+++ psk.bio.car.rental.infrastructure/src/main/java/psk/bio/car/rental/infrastructure/data/reservation/ReservationEntity.java
@@ -0,0 +1,5 @@
+package psk.bio.car.rental.infrastructure.data.reservation;
+
+public class ReservationEntity {
+
+}
