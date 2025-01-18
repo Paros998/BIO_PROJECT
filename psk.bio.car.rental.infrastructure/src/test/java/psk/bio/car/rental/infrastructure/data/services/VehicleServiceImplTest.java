51a1c3c5380256ea52d1b048d07d98013c7a3745
komob <b.komorniczak@gmail.com>
2025-01-18 13:52:53 +0100

    improved vehicle service test

--- psk.bio.car.rental.infrastructure/src/test/java/psk/bio/car/rental/infrastructure/data/services/VehicleServiceImplTest.java
+++ psk.bio.car.rental.infrastructure/src/test/java/psk/bio/car/rental/infrastructure/data/services/VehicleServiceImplTest.java
@@ -8,8 +8,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+
+import org.springframework.web.server.ResponseStatusException;
 import psk.bio.car.rental.api.common.paging.PageResponse;
 import psk.bio.car.rental.api.vehicles.VehicleModel;
+import psk.bio.car.rental.application.vehicle.VehicleState;
 import psk.bio.car.rental.infrastructure.data.vehicle.VehicleEntity;
 import psk.bio.car.rental.infrastructure.data.vehicle.VehicleJpaRepository;
 
@@ -30,20 +33,14 @@ class VehicleServiceImplTest {
     @Test
     void shouldReturnEmptyPageResponseWhenNoVehiclesFound() {
         // Arrange
-        var pageRequest = new psk.bio.car.rental.api.common.paging.PageRequest(0, 10, null, null);
-        var springPageRequest = PageRequest.of(0, 10);
-
-        // Mockujemy pusty wynik
-        Page<VehicleEntity> emptyPage = new PageImpl<>(Collections.emptyList(), springPageRequest, 0);
-        when(vehicleRepository.findAll(springPageRequest)).thenReturn(emptyPage);
+        var pageRequest = new psk.bio.car.rental.api.common.paging.PageRequest(0, 10, "asc", "dir");
 
         // Act
-        PageResponse<VehicleModel> result = vehicleService.searchVehicles(null, pageRequest);
-
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            vehicleService.searchVehicles(VehicleState.NEW, pageRequest);
+        });
         // Assert
-        assertNotNull(result);
-        assertTrue(result.getContent().isEmpty());
-        assertEquals(0, (int) result.getTotalPages()); // Używamy istniejących metod
-        assertEquals(0, (int) result.getCurrentPage());
+        assertEquals(400, exception.getStatusCode().value());
+        assertEquals("No member with class dir exists on this entity", exception.getReason());
     }
 }
